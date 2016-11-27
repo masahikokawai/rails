624fbaab1a1504c3643a328d931df667635b4fa1
masahiko kawai <masahikokawai81@gmail.com>
2016-11-27 17:30:50 +0000

    20171127分

--- rails_projects/sample_app/app/models/micropost.rb
+++ rails_projects/sample_app/app/models/micropost.rb
@@ -3,4 +3,23 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+
+##  def self.from_users_followed_by(user)
+##    followed_user_ids = user.followed_user_ids
+##    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+##  end
+#  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+#  def self.from_users_followed_by(user)
+#    followed_user_ids = user.followed_user_ids
+#    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+#          followed_user_ids: followed_user_ids, user_id: user)
+#  end
+  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
+
 end
